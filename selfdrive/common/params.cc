46e68803b3e81c846330b7f00eea3c9baa0c4aee
Maximammx <89095658+Maximammx@users.noreply.github.com>
2021-09-06 10:56:43 -0700

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -123,6 +123,7 @@ private:
 
 std::unordered_map<std::string, uint32_t> keys = {
     {"AccessToken", CLEAR_ON_MANAGER_START | DONT_LOG},
+    {"LatKpv", PERSISTENT},
     {"ApiCache_DriveStats", PERSISTENT},
     {"ApiCache_Device", PERSISTENT},
     {"ApiCache_Owner", PERSISTENT},

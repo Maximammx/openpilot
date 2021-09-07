088166f537d0e6cb068c868c0e13af2f210ad580
Maximammx <89095658+Maximammx@users.noreply.github.com>
2021-09-06 21:44:37 -0700

    Update ssh_keys.cc

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -72,9 +72,6 @@ latkiv::latkiv() : AbstractControl("Lat Kpv", "Description here", "../assets/off
     auto str = QString::fromStdString(params.get("LatKiv"));
     int value = str.toInt();
     value = value - digit;
-    if (value <= 0.1) {
-      value = 0.1;
-    }
     QString values = QString::number(value);
     params.put("LatKiv", values.toStdString());
     refresh();

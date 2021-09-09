2585dc35c0aea0fcc82776bfe1d5a2a095580eb1
Maximammx <89095658+Maximammx@users.noreply.github.com>
2021-09-08 19:55:46 -0700

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -206,8 +206,8 @@ static void ui_draw_vision_lat(UIState *s) {
   ui_draw_rect(s->vg, rect, COLOR_WHITE_ALPHA(100), 10, 20.);
 
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
-  ui_draw_text(s, rect.centerX(), 118, Params.get("LatKiv"), 48 * 2.5, COLOR_WHITE, "sans-bold");
-  ui_draw_text(s, rect.centerX(), 212, Params.get("LatKpv"), 48 * 2.5, COLOR_WHITE, "sans-bold");
+  ui_draw_text(s, rect.centerX(), 118, Params().get("LatKiv"), 48 * 2.5, COLOR_WHITE, "sans-bold");
+  ui_draw_text(s, rect.centerX(), 212, Params().get("LatKpv"), 48 * 2.5, COLOR_WHITE, "sans-bold");
 }
 
 static void ui_draw_vision_speed(UIState *s) {
